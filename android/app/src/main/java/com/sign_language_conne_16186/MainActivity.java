f1cb4a981aa0b032b63dd4ddbc4cf7f557cb6a2f
Crowdbotics <team@crowdbotics.com>
2020-04-23 17:36:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sign_language_conne_16186/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sign_language_conne_16186;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sign_language_conne_16186";
+    }
+}
